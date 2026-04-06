53902550b2a2862904833725dba49ebde2be71a4
Sergio Jauregui <sergioja2003@gmail.com>
2026-04-06 16:27:28 -0500

    Actualización de dependencias: Renombrado proyecto y actualización de paquetes

--- Frontend/postcss.config.js
+++ Frontend/postcss.config.js
@@ -1,5 +1,7 @@
 export default {
   plugins: {
+    // postcss-nesting MUST come before tailwindcss to process nested CSS from swiper
+    'postcss-nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
